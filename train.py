f9c652838c1996e267efa6f017ab23bd4b5cd5e4
snowclipsed <harrybishnoi@gmail.com>
2025-09-12 18:22:05 -0400

    change hparams for faster training on a100

--- train.py
+++ train.py
@@ -18,21 +18,23 @@ env = vf.load_environment(
 run_name = "rubiks-cube-grpo-qwen3b"
 training_args = vf.grpo_defaults(run_name=run_name)
 
-training_args.per_device_train_batch_size = 4
+training_args.per_device_train_batch_size = 8
 training_args.num_generations = 8
 training_args.gradient_accumulation_steps = 4
-training_args.max_tokens = 2048
+training_args.max_tokens = 1024
 training_args.max_seq_len = 2048
-training_args.max_steps = 500
+
+training_args.max_steps = 30
 training_args.eval_strategy = "steps"
-training_args.eval_steps = 10
-training_args.save_steps = 100
-training_args.logging_steps = 5
+training_args.eval_steps = 2
+training_args.save_steps = 10
+training_args.logging_steps = 1
+
 training_args.mask_env_responses = True
 training_args.max_grad_norm = 0.1
-training_args.beta = 0.0
+training_args.beta = 0.01
 training_args.async_generation_timeout = 600
-training_args.learning_rate = 1e-6
+training_args.learning_rate = 2e-6
 training_args.warmup_ratio = 0.1
 training_args.fp16 = True
 training_args.gradient_checkpointing = True
